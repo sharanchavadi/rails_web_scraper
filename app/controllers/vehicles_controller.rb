a149d17a2f6850265e380e3e2bdf1fff30e6a2c2
sharanchavadi <sharanschavadi@gmail.com>
2020-05-07 14:15:04 +0530

    Added scrape button to view and scrape action to vehicles controller

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -60,6 +60,18 @@ class VehiclesController < ApplicationController
       format.json { head :no_content }
     end
   end
+  
+  def scrape
+    url = 'https://www.cars.com/shopping/suv/'
+    response = VehiclesSpider.process(url)
+    if response[:status] == :completed && response[:error].nil?
+      flash.now[:notice] = "Successfully scraped url"
+    else
+      flash.now[:alert] = response[:error]
+    end
+    rescue StandardError => e
+      flash.now[:alert] = "Error: #{e}"
+  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
